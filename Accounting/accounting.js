cd30971a84034979ec08e79d04cec5a702ee44eb
iljaaz <iljashishin@gmail.com>
2020-03-11 14:28:32 +0200

    changes

--- Accounting/accounting.js
+++ Accounting/accounting.js
@@ -1,15 +1,15 @@
 const MAX_EXPENSES = 150;
 let currentExpenses = 0;
 
-function addExpense(description, amount) {
+function addExpense(description, price) {
   if (amount < 0) {
     return false;
   } else {
 
   }
 
-  if (currentExpenses + amount <= MAX_EXPENSES) {
-    currentExpenses += amount;
+  if (currentExpenses + price <= MAX_EXPENSES) {
+    currentExpenses += price;
     return true;
   } else {
     return false;
